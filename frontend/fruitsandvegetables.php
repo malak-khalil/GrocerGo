fe8e63c3ece32c78f1b28bd840488d7395ad0989
malak-khalil <malak.khalil@lau.edu>
2025-04-21 20:54:47 +0300

    items

--- frontend/fruitsandvegetables.php
+++ frontend/fruitsandvegetables.php
@@ -100,6 +100,7 @@
 </div>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script>
+        
         $(document).ready(function() {
     $.ajax({
         url: '../backend/get-fruitsandvegetables.php',
