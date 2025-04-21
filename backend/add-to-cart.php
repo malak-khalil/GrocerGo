23c13dd431bb32e01a01c82afe4a6b2505bd513d
rnightsky <reina.najjar9@gmail.com>
2025-04-21 16:49:07 +0300

    add to cart backend in one file

---
+++ backend/add-to-cart.php
@@ -0,0 +1,32 @@
+<?php   // Reina Najjar
+$host = "localhost";
+$user = "root";
+$password = "";
+$dbname = "grocergo";
+
+$conn = mysqli_connect($host, $user, $password, $dbname);
+
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+$user_id = 1;
+
+if(isset($_POST['add_to_cart'])) {
+    $product_id = $_POST['product_id'];
+    $product_name = mysqli_real_escape_string($conn, $_POST['product_name']);
+    $product_price = $_POST['product_price'];
+    $product_image = $_POST['product_image'];
+    $product_quantity = $_POST['product_quantity'];
+    
+    $select_cart = mysqli_query($conn, "SELECT * FROM `cart` WHERE product_id = '$product_id' AND user_id = '$user_id'") or die('query failed');
+
+    if(mysqli_num_rows($select_cart) > 0){
+        mysqli_query($conn, "UPDATE cart SET quantity='$product_quantity' WHERE product_id = '$product_id' AND user_id = '$user_id'");
+    }else{
+        mysqli_query($conn, "INSERT INTO cart (user_id, product_id, product_name, product_img, product_price, quantity) VALUES('$user_id', '$product_id', '$product_name', '$product_image', '$product_price', '$product_quantity')") or die('query failed');
+    }
+}
+ 
+
+?>
