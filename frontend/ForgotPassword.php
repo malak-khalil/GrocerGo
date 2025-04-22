40acd32db936c8b3fa9f2e828ba01c9925eb4116
Antonio Karam <antonio.karam@lau.edu>
2025-04-22 12:10:34 +0300

    fixed reloading when forgot password

--- frontend/ForgotPassword.php
+++ frontend/ForgotPassword.php
@@ -1,11 +1,5 @@
 <!-- By Antonio Karam --> 
-<?php
-session_start();
-if (!isset($_SESSION['user_id'])) {
-    header("Location: ../frontend/Log-in.php"); 
-    exit();
-}
-?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
